9b22e9efff6cfe49a53b1fcda680bea02d7499de
iam-jmismail <jmismail628@gmail.com>
2021-09-08 08:28:27 +0530

    environment

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./env" });
+
+export const PORT: number | string = process.env.PORT || 3000;
+export const PG_USERNAME: string = process.env.PG_USERNAME || "root";
+export const PG_PASSWORD: string = process.env.PG_PASSWORD || "toor";
+export const PG_DATABASE: string = process.env.PG_DATABASE || "sample";
+export const PG_HOST: string = process.env.PG_HOST || "localhost";
